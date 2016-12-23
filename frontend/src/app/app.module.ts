124cd38b07f9204a328c9cdf23c098ff47a32843
karszczak <daniel.karski@gmail.com>
2016-12-23 19:37:43 +0100

    set router

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
-import { UploadFileComponent } from './upload-file/upload-file.component';
+import { AppRoutingModule }        from './app-routing.module';
+
+import { PageNotFoundComponent } from './not-found.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UploadFileComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HomeModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
