8f0a301854bb4a5ae32fec08a8add3d91133c391
karszczak <daniel.karski@gmail.com>
2016-12-27 09:57:40 +0100

    add delate Router

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,23 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule }        from './app-routing.module';
-
-import { PageNotFoundComponent } from './not-found.component'
-
-import { HomeModule } from './home/home.module';
+import { FormUploadComponent } from './form-upload/form-upload.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HomeModule,
-    AppRoutingModule,
     ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
+    FormUploadComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'home',
-  templateUrl: './home.component.html'
-  // styleUrls: ['./form-upload.component.scss']
-})
-export class HomeComponent { }
